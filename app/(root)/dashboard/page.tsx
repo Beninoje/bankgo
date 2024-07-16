bbd9296bb746ba5ce0236cd1340f5d62414acf4d
Beni <beninoje@hotmail.com>
2024-07-15 21:23:47 -0400

    added chartJS npm package

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -1,8 +1,28 @@
+import DashboardHeader from '@/components/DashboardHeader'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Dashboard = () => {
+  const loggedIn = {firstName:'Beni'}
   return (
-    <div>Dashboard</div>
+    <section className='home'>
+       <div className="home-content">
+        <header className="home-header">
+          <DashboardHeader
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.32}
+          />
+        </header>
+       </div>
+    </section>
   )
 }
 
