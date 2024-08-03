62a226ba050f9e7abd4cc399706c69fd52669d4d
Beni <beninoje@hotmail.com>
2024-08-03 17:14:09 -0400

    added sliding header and hamburger

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,40 +1,70 @@
+"use client"
 import { headerLinks } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import HeaderLinks from './HeaderLinks'
+import { useEffect, useState } from 'react'
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isScrollingUp, setIsScrollingUp] = useState(true);
+  let lastScrollY = 0;
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY < lastScrollY) {
+        setIsScrollingUp(true);
+      } else {
+        setIsScrollingUp(false);
+      }
+      lastScrollY = window.scrollY;
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }, [isOpen]);
+  
   return (
     <div className='px-3 py-4 w-full'>
-      <nav className="nav_container">
+        <nav className={`nav_container ${isScrollingUp ? 'visible' : 'hidden'}`}>
         <Link href="/" className="flex gap-2 items-center">
-          <Image src="/icons/logo.svg"
-            width={30}
-            height={30}
-            alt="bankon logo"/>
-            <h1 className='text-color font-semibold text-[24px] tracking-wide'>bank<span className='text-[#FF7E61]'>on</span></h1>
+          <Image src="/icons/logo.svg" width={30} height={30} alt="bankon logo" />
+          <h1 className="text-color font-semibold text-[24px] tracking-wide">
+            bank<span className="text-[#FF7E61]">on</span>
+          </h1>
         </Link>
-        <div className="">
-          <ul className='nav_list '>
-            {headerLinks.map((item,i)=>(
-              <HeaderLinks
-                key={i}
-                title={item.title}
-                link={item.link}
-              />
+        <div className={`nav_list_container ${isOpen ? 'open' : ''}`}>
+          <ul className="nav_list">
+            {headerLinks.map((item, i) => (
+              <HeaderLinks key={i} title={item.title} link={item.link} />
             ))}
           </ul>
         </div>
         <div className="auth_btn_container">
-            <Link href="/sign-in" className='mx-4'>
-              <span className='login_in_btn'>Login</span>
-            </Link>
-            <Link href="/sign-up" className='sign_up_btn'>
-              Sign Up
-            </Link>
+          <Link href="/sign-in" className="mx-4">
+            <span className="login_in_btn">Login</span>
+          </Link>
+          <Link href="/sign-up" className="sign_up_btn">
+            Sign Up
+          </Link>
         </div>
+        <button className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+          <span className="bar bar-1"></span>
+          <span className="bar bar-2"></span>
+          <span className="bar bar-3"></span>
+        </button>
       </nav>
-      
     </div>
   )
 }
