85b3306324426e0e77e4d6afcf4b5be26f4698c6
Beni <beninoje@hotmail.com>
2024-08-16 13:09:24 -0400

    auth pages UI done

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 
 
 export default function RootLayout({
@@ -6,8 +7,13 @@ export default function RootLayout({
     children: React.ReactNode;
   }>) {
     return (
-      <main>
+      <main className="flex min-h-screen w-full justify-between">
           {children}
+          <div className="auth-asset overflow-hidden">
+            <div className=" pl-20 mr-[-100px] rounded-[20px]">
+              <Image src="/icons/auth_img.svg" alt="homepage img" width={750} height={750} className=' border-4 border-[#FF7E61] rounded-[20px]'/>
+            </div>
+          </div>
       </main>
     );
   }
