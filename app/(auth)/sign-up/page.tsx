58ec78c44020fe66cd7ad5998a8e206caa38538f
Noje <Benjamin.Noje@york.ca>
2024-08-27 15:11:16 -0400

    added logical routing

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,12 @@
 import AuthForm from '@/components/AuthForm'
+import { getLoggedInUser } from '@/lib/actions/user.actions';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
 const SignUp = async ()  => {
+  const loggedIn = await getLoggedInUser();
 
+  if(loggedIn) redirect('/dashboard');
   return (
     <section className="flex-center size-full max-sm:px-6">
       <AuthForm type="sign-up"/>
