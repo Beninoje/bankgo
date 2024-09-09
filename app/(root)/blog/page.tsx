63d23dca9c6608b7be7e9c4783f076da7f24d0a1
Beni Noje <haiducule@Benis-MacBook-Air.local>
2024-09-09 17:06:49 -0400

    added blog structure on componenet

--- app/(root)/blog/page.tsx
+++ app/(root)/blog/page.tsx
@@ -1,8 +1,41 @@
+import Footer from '@/components/Footer';
+import Header from '@/components/Header'
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 import React from 'react'
 
-const Blog = () => {
+const Blog = async () => {
+  const user = await getLoggedInUser();
   return (
-    <div>Blog</div>
+    <div className='flex flex-col'>
+      <Header user={user}/>
+      <div className="w-full h-[300px] pt-[100px] bg-[#22222E]"></div>
+      <div className="flex justify-center gap-3">
+        <div className="article_section">
+          <div className="image_section">
+
+          </div>
+          <div className="authors">
+
+          </div>
+          <div className="bankon_promise">
+
+          </div>
+          <div className="header-1">
+
+          </div>
+          <div className="paragraph">
+
+          </div>
+          <div className="written_by">
+
+          </div>
+        </div>
+        <div className="article_director">
+          
+        </div>
+      </div>
+      <Footer/>
+    </div>
   )
 }
 
